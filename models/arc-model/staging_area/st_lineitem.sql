270d3fb588cfd198b0e6eaeacf79d1021b834b50
Alok Ranjan Mishra <xdxd25022@gmail.com>
2025-02-09 18:26:59 +0000

    add st_lineitem model

---
+++ models/arc-model/staging_area/st_lineitem.sql
@@ -0,0 +1,23 @@
+{{ config(
+    materialized='incremental', 
+    unique_key='lineitem_id',
+    incremental_strategy='merge'
+) }}
+
+    SELECT 
+        CONCAT(l.l_orderkey, '-', l.l_linenumber) AS lineitem_id,  -- Composite key
+        l.l_orderkey AS order_id,
+        l.l_partkey AS part_id,
+        l.l_suppkey AS supplier_id,
+        CAST(COALESCE(l.l_quantity, 0) AS INTEGER) AS quantity,
+        l.l_discount AS discount,
+        l.l_extendedprice AS revenue,
+        l.l_shipdate AS ship_date,
+        l.l_commitdate AS commit_date,
+        l.l_receiptdate AS receipt_date
+    FROM {{ source('landing', 'LINEITEM') }} l
+    {% if is_incremental() %}
+    WHERE l.l_shipdate > (SELECT MAX(ship_date) FROM {{ this }})  -- Load only new records
+    {% endif %}
+    
+

ef8eac99e3348fb780af62dcfa880f29f628e2bb
Alok Ranjan Mishra <xdxd25022@gmail.com>
2025-02-10 05:27:46 +0000

    error fix/

--- models/arc-model/staging_area/st_order.sql
+++ models/arc-model/staging_area/st_order.sql
@@ -9,7 +9,9 @@ SELECT
     o.o_custkey AS customer_id,
     o.o_orderdate AS order_date,
     o.o_totalprice AS total_price,
-    o.o_orderstatus AS order_status
+    o.o_orderstatus AS order_status,
+    o.O_ORDERPRIORITY AS order_priority,
+    o.o_shippriority AS ship_priority
 FROM {{ source('landing', 'ORDERS') }} o
 
 {% if is_incremental() %}
